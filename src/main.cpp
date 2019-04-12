4bdba87a996ce3e8f18a55baa33935ef71ad760a
Egor Ananko <egor_ananko@mail.ru>
2019-04-12 22:35:18 +0300

    Preparing for kernel execution & results comparing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+#include <chrono>
 #include <iostream>
 #include <memory>
 
@@ -11,38 +12,84 @@
 typedef short data_t;
 
 int main(int argc, char const *argv[]) {
-    data_t *d_data;
-    data_t *h_data;
-    size_t rows = 10;
-    size_t cols = 10;
+    data_t *d_data_in;
+    data_t *h_data_in;
+    data_t *d_data_out;
+    data_t *h_data_out;
+    size_t rows = 1024;
+    size_t cols = 1024;
+
+    {
+        size_t rows_tmp;
+        size_t cols_tmp;
+        if (argc > 2) {
+            try {
+                rows_tmp = std::stoul(argv[1]);
+                cols_tmp = std::stoul(argv[2]);
+                rows = rows_tmp;
+                cols = cols_tmp;
+            } catch (const std::invalid_argument &e) {
+                std::cerr << e.what() << ": invalid argument" << '\n';
+            } catch (const std::out_of_range &e) {
+                std::cerr << e.what() << ": out of range" << '\n';
+            }
+        }
+    }
+
+    std::cout << "Rows: " << rows << std::endl
+              << "Cols: " << cols << std::endl;
+
     size_t len = rows * cols;
     size_t size_in_bytes = len * sizeof(data_t);
 
-    h_data = (data_t *)calloc(len, sizeof(data_t));
-    cudaMalloc(&d_data, size_in_bytes);
-    random_init(d_data, size_in_bytes);
+    std::cout << "len: " << len << std::endl;
+    std::cout << "sib: " << size_in_bytes << std::endl;
 
-    // cudaEvent_t start;
-    // cudaEvent_t stop;
-    // cudaEventCreate(&start);
-    // cudaEventCreate(&stop);
+    h_data_in = (data_t *)calloc(len, sizeof(data_t));
+    h_data_out = (data_t *)calloc(len, sizeof(data_t));
+    cudaMalloc(&d_data_in, size_in_bytes);
+    random_init(d_data_in, size_in_bytes);
+    cudaMalloc(&d_data_out, size_in_bytes);
 
-    // cudaEventRecord(start, 0);
+    cudaEvent_t e_start;
+    cudaEvent_t e_stop;
+    cudaEventCreate(&e_start);
+    cudaEventCreate(&e_stop);
 
-    // cudaEventRecord(stop, 0);
+    dim3 threads_per_block{32 * 8};
+    dim3 num_blocks{(uint)(cols / 2 / 32),
+                    (uint)(rows / 2 / 32 / 8)};  // TODO: change for all dims
 
-    // cudaEventSynchronize(stop);
-    // float elapsedTime;
-    // cudaEventElapsedTime(&elapsedTime, start, stop);
+    cudaEventRecord(e_start, 0);
+    launch_kernel(threads_per_block, num_blocks, (uint *)d_data_in, (uint *)d_data_out, rows, cols);
+    cudaEventRecord(e_stop, 0);
 
-    cudaMemcpy(h_data, d_data, size_in_bytes, cudaMemcpyDeviceToHost);
+    cudaEventSynchronize(e_stop);
+    float elapsedTimeGPU;
+    cudaEventElapsedTime(&elapsedTimeGPU, e_start, e_stop);
+
+    std::cout << "[GPU] Elapsed time: " << elapsedTimeGPU << " ms" << std::endl;
+
+    cudaMemcpy(h_data_in, d_data_in, size_in_bytes, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_data_out, d_data_out, size_in_bytes, cudaMemcpyDeviceToHost);
+
+    auto c_start = std::chrono::steady_clock::now();
+    auto cpu_data = cpu_transform(h_data_in, rows, cols);
+    auto c_stop = std::chrono::steady_clock::now();
+    auto elapsedTimeCPU = std::chrono::duration<float, std::milli>(c_stop - c_start).count();
+
+    std::cout << "[CPU] Elapsed time: " << elapsedTimeCPU << " ms" << std::endl;
+
     /* for (size_t i = 0; i < len; i++) {
-        std::cout << h_data[i] << std::endl;
+        std::cout << cpu_data[i] << std::endl;
     } */
 
-    free(h_data);
-    cudaFree(d_data);
-    // cudaEventDestroy(start);
-    // cudaEventDestroy(stop);
+    free(h_data_in);
+    free(h_data_out);
+    cudaFree(d_data_in);
+    cudaFree(d_data_out);
+    cudaEventDestroy(e_start);
+    cudaEventDestroy(e_stop);
+    cudaDeviceReset();
     return 0;
 }
