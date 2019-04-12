02d9202301f25b9ce395ffe4d1a921519e511535
Egor Ananko <egor_ananko@mail.ru>
2019-04-12 22:33:37 +0300

    Added kernel definition & launching func

--- src/cuda_kernel.cu
+++ src/cuda_kernel.cu
@@ -4,5 +4,15 @@
 
 #include "cuda_kernel.cuh"
 
-__global__ void kernel() {
+__global__ void mirror_rotate_kernel(uint* src, uint* dst,
+                                     size_t rows, size_t cols) {
+}
+
+__host__ void launch_kernel(dim3 threads_per_block,
+                            dim3 num_blocks,
+                            uint* in,
+                            uint* out,
+                            size_t rows,
+                            size_t cols) {
+    mirror_rotate_kernel<<<num_blocks, threads_per_block>>>(in, out, rows, cols);
 }
--- src/cuda_kernel.cuh
+++ src/cuda_kernel.cuh
@@ -5,4 +5,12 @@
 
 typedef short data_t;
 
-__global__ void kernel();
+__global__ void mirror_rotate_kernel(uint *src, uint *dst,
+                                     size_t rows, size_t cols);
+
+__host__ void launch_kernel(dim3 threads_per_block,
+                            dim3 num_blocks,
+                            uint* in,
+                            uint* out,
+                            size_t rows,
+                            size_t cols);
