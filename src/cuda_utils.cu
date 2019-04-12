c26da98aaaeb6c78afbc819aa1f58a4f1b810c0e
Egor Ananko <egor_ananko@mail.ru>
2019-04-12 22:31:19 +0300

    Fix segmentation fail

--- src/cuda_utils.cu
+++ src/cuda_utils.cu
@@ -4,13 +4,13 @@
 
 #include "cuda_utils.cuh"
 
-__host__ void random_init(data_t *data,
+__host__ void random_init(data_t* data,
                           size_t size_in_bytes,
                           unsigned long long seed) {
     curandGenerator_t gen;
     curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
     curandSetPseudoRandomGeneratorSeed(gen, seed);
-    curandGenerate(gen, (unsigned int *)data,
-                   size_in_bytes / (sizeof(unsigned int) / sizeof(data_t)));
+    curandGenerate(gen, (unsigned int*)data,
+                   size_in_bytes / sizeof(unsigned int));
     curandDestroyGenerator(gen);
 }
