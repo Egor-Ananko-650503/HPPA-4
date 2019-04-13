a614af1c576f52d4d369a083f4208a0b486c8e7c
Egor Ananko <egor_ananko@mail.ru>
2019-04-14 01:20:27 +0300

    Correct read and write transactions

--- src/cuda_kernel.cu
+++ src/cuda_kernel.cu
@@ -6,6 +6,45 @@
 
 __global__ void mirror_rotate_kernel(uint* src, uint* dst,
                                      size_t rows, size_t cols) {
+    // Allocate shared memory for block
+    __shared__ uint smem[32 * 32 * 8];
+    // Shift to shared memory for current warp
+    uint* smem_ = &smem[32 * 32 * threadIdx.y];
+
+    // x, y for current thread on global memory relatively blocks
+    dim3 src_offset{blockIdx.x * 32,
+                    blockIdx.y * 32 * 8 + threadIdx.y * 32};
+
+    size_t length = rows * (cols / 2);
+    size_t src_idx = src_offset.y * (cols / 2) + src_offset.x;
+
+    if (src_idx < length) {
+        // Shift to global memoty for current thread
+        uint* src_ = &src[src_offset.y * (cols / 2) + src_offset.x];
+
+        // Load to shared memory
+        for (size_t i = 0; i < 32; i++) {
+            smem_[threadIdx.x + i * 32] = src_[threadIdx.x + i * (cols / 2)];
+        }
+
+        // __syncthreads();
+
+        // Swap 2 shorts on uint [2B_1 2B_2] -> ]2B_2 2B_1]
+        for (size_t i = 0; i < 32; i++) {
+            data_t* pair = (data_t*)&smem_[threadIdx.x + i * 32];
+            data_t tmp = pair[0];
+            pair[0] = pair[1];
+            pair[1] = tmp;
+        }
+
+        uint* dst_ = &dst[src_offset.y * (cols / 2) + src_offset.x];
+
+        for (size_t i = 0; i < 32; i++) {
+            dst_[threadIdx.x + i * (cols / 2)] = smem_[threadIdx.x + i * 32];
+        }
+
+        // __syncthreads();
+    }
 }
 
 __host__ void launch_kernel(dim3 threads_per_block,
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <cuda_runtime.h>
 
 #include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <memory>
+#include <vector>
 
 #include "cpu_transform.hpp"
 #include "cuda_kernel.cuh"
@@ -56,9 +58,18 @@ int main(int argc, char const *argv[]) {
     cudaEventCreate(&e_start);
     cudaEventCreate(&e_stop);
 
-    dim3 threads_per_block{32 * 8};
-    dim3 num_blocks{(uint)(cols / 2 / 32),
-                    (uint)(rows / 2 / 32 / 8)};  // TODO: change for all dims
+    dim3 threads_per_block{32, 8};
+    dim3 num_blocks{(uint)((cols / 2 + 31) / 32),
+                    (uint)((rows + (32 * 8 - 1)) / (32 * 8))};  // TODO: change for all dims
+
+    std::cout << "Threads per block: "
+              << threads_per_block.x << ' '
+              << threads_per_block.y << ' '
+              << threads_per_block.z << std::endl
+              << "Blocks: "
+              << num_blocks.x << ' '
+              << num_blocks.y << ' '
+              << num_blocks.z << std::endl;
 
     cudaEventRecord(e_start, 0);
     launch_kernel(threads_per_block, num_blocks, (uint *)d_data_in, (uint *)d_data_out, rows, cols);
@@ -73,6 +84,48 @@ int main(int argc, char const *argv[]) {
     cudaMemcpy(h_data_in, d_data_in, size_in_bytes, cudaMemcpyDeviceToHost);
     cudaMemcpy(h_data_out, d_data_out, size_in_bytes, cudaMemcpyDeviceToHost);
 
+    // Testing
+    {
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j += 2) {
+                data_t tmp = std::move(h_data_in[i * cols + j]);
+                h_data_in[i * cols + j] = std::move(h_data_in[i * cols + j + 1]);
+                h_data_in[i * cols + j + 1] = std::move(tmp);
+            }
+        }
+
+        // std::vector<std::pair<size_t, size_t>> v_er;
+        size_t errors_count = 0;
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                if (h_data_in[i * cols + j] != h_data_out[i * cols + j]) {
+                    errors_count++;
+                    // v_er.push_back(std::move(std::make_pair(i, j)));
+                }
+            }
+        }
+        std::cout << "Errors: " << errors_count << std::endl;
+
+        // for (size_t i = 0; i < 100; i++) {
+        //     auto i_ = v_er.at(i).first;
+        //     auto j_ = v_er.at(i).second;
+        //     std::cout << i_ << ' ' << j_ << " - "
+        //               << h_data_in[i_ * cols + j_] << ' '
+        //               << h_data_out[i_ * cols + j_] << std::endl;
+        // }
+
+        // for (size_t i = 0; i < 0 + 32; i++) {
+        //     for (size_t j = 0; j < 32; j++) {
+        //         std::cout << std::setw(6) << h_data_in[i * cols + j] << ' ';
+        //     }
+        //     std::cout << std::endl;
+        //     for (size_t j = 0; j < 32; j++) {
+        //         std::cout << std::setw(6) << h_data_out[i * cols + j] << ' ';
+        //     }
+        //     std::cout << "\n\n";
+        // }
+    }
+
     auto c_start = std::chrono::steady_clock::now();
     auto cpu_data = cpu_transform(h_data_in, rows, cols);
     auto c_stop = std::chrono::steady_clock::now();
@@ -80,9 +133,16 @@ int main(int argc, char const *argv[]) {
 
     std::cout << "[CPU] Elapsed time: " << elapsedTimeCPU << " ms" << std::endl;
 
-    /* for (size_t i = 0; i < len; i++) {
-        std::cout << cpu_data[i] << std::endl;
-    } */
+    // size_t errors_count = 0;
+    // for (size_t i = 0; i < rows; i++) {
+    //     for (size_t j = 0; j < cols; j++) {
+    //         if (cpu_data[i * cols + j] != h_data_out[i * cols + j]) {
+    //             errors_count++;
+    //         }
+    //     }
+    // }
+
+    // std::cout << "Errors: " << errors_count << std::endl;
 
     free(h_data_in);
     free(h_data_out);
